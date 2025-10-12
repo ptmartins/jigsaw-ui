5a451002bf183b89ccb833ad9164e3b556d84b40
Pedro Martins <pedro@pedromartins.com.pt>
2025-10-12 11:22:40 +0100

    Add progress bar component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { JGAvatar } from './jg-avatar.js';
 export { JGButton } from './jg-button.js';
-export { JGIcon } from './jg-icon.js';
 export { JGGroup } from './jg-group.js';
+export { JGIcon } from './jg-icon.js';
+export { JGProgress } from './jg-progress.js';
